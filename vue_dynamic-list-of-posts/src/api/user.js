b740b1854a79539223212a38df074840f0d41a57
Tetiana Veremchuk <tetianaverem@gmail.com>
2024-08-28 20:39:27 +0300

    Deploy to GitHub Pages

--- vue_dynamic-list-of-posts/src/api/user.js
+++
@@ -1,9 +0,0 @@
-import { client } from "../utils/httpClient";
-
-export const login = (email) => {
-  return client.get(`/users?email=${email}`);
-};
-
-export const register = (email, name) => {
-  return client.post(`/users`, { email, name });
-};
